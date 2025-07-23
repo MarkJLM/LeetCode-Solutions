c8cd7c10359a8b3ab6a26063e7b8b1054081f41e
Mark <mjlm53106@gmail.com>
2025-07-23 17:15:58 -0500

    Create #1290

---
+++ 1290. Convert Binary Number in a Linked List to Integer.js
@@ -0,0 +1,22 @@
+/* Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+Return the decimal value of the number in the linked list.
+The most significant bit is at the head of the linked list. */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    var output = 0;
+    while (head.next != null) {
+        output = output * 2 + head.val;
+        head = head.next;
+    }
+    return output * 2 + head.val;
+};
